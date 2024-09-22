a2e5c938a9a7aeabdd959d8746d5ba7dc9d1fb70
Juliana Quaresma <julianaqribeiro@hotmail.com>
2024-09-22 19:04:47 -0300

    Ajuste do nome da API

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from model.reserva import Reserva
 from schemas import *
 from flask_cors import CORS
 
-info = Info(title="API Módulo Full Stack Básico", version="1.0.0")
+info = Info(title="API Restaurante", version="1.0.0")
 app = OpenAPI(__name__, info=info)
 CORS(app)
 
